b5dc3ffc15d1919d2406fd528a324744a7c6c3d2
Andrew <amasek@purdue.edu>
2020-05-28 10:48:19 -0400

    Add working slideshow mechanic

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+var slideIndex = 1;
+
+/**
+ * Applies number received from next/prev buttons (1 or -1) to show next or previous slide
+ * from the current slide index.
+ */
+function moveSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/**
+ * Shows slide at slideIndex and checks/adjusts if index is out of bounds.
+ */
+function showSlides(n) {
+    let i;
+    const slides = document.querySelectorAll('.slides');
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
 /**
  * Expands a collapsible when clicked.
  */
